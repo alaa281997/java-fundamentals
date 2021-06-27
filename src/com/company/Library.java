70d5e16b4f615cf56ab37b895abfc2cba26120ec
alaa281997 <alaabbas97@gmail.com>
2021-06-28 00:07:44 +0300

    finished

---
+++ src/com/company/Library.java
@@ -0,0 +1,92 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Library {
+    public static void main(String[] args) {
+
+        roll(4);
+        int[][] arrr = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+        int x[] = {1,2,3,4};
+       System.out.println("Result = "+ containsDuplicates(x));
+        System.out.println("Avg = "+ average(x));
+        System.out.println("The Avg = " + arrOfArr(arrr));
+    }
+    public static void roll(int n){
+      int min=1,max=7;
+        Random random = new Random();
+        ArrayList<Integer> dice = new ArrayList<Integer>();
+        for (int i = 0; i < n ; i++) {
+           dice.add(random.nextInt(max-min)+1) ;
+
+        }System.out.println(dice);
+
+
+
+    }
+    static boolean containsDuplicates(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+   static double average(int[] arrInt){
+       double sum = 0;
+        double avg = 0;
+       for (int i = 0; i < arrInt.length; i++) {
+           sum = sum +arrInt[i];
+          avg = sum/arrInt.length;
+       }
+       return avg;
+   }
+ static int arrOfArr(int[][] arr){
+    int min=0;
+    for (int i = 0; i < arr.length; i++) {
+        int sum = 0;
+        for (int j = 0; j < arr[i].length; j++) {
+            sum = arr[i][j] + 1;
+            System.out.println(arr[i][j]);
+        }
+        int avg = sum/ arr[i].length;
+        min = sum/arr[0].length;
+        if(min>avg){
+            min = avg;
+        }
+
+    }
+   return  min;
+}
+
+/*
+public static void printtda() {
+
+        for (int i=0; i<weeklyMonthTemperatures.length; i++) {
+            int sum = 0;
+            for (int j=0; j< weeklyMonthTemperatures[i].length; j++) {
+                sum+=weeklyMonthTemperatures[i][j];
+//                System.out.print(weeklyMonthTemperatures[i][g] +"  ");
+            }
+            int avgEveryRow =  sum/weeklyMonthTemperatures[i].length;
+            min = sum/weeklyMonthTemperatures[0].length;
+            if (min > avgEveryRow){
+                min = avgEveryRow;
+            }
+            System.out.println("avg every row "+avgEveryRow);
+        }
+        System.out.println(min);
+    }
+ */
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,63 @@
 package com.company;
+import java.util.ArrayList;
+import java.util.Random;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int dogCount = 1;
+        System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
 
+        int catCount = 2;
+        System.out.println("I own " + catCount + " " + pluralize("cat", catCount) + ".");
 
+        int turtleCount = 0;
+        System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
 
     }
+    public static String pluralize(String name,int num){
+        if(num > 1 || num == 0){
+            return name + "s";
+        }else {
+            return name;
+        }
+    }
+
+    public static void flipNHeads(int n){
+        int fliping=0, heads = 0;
+        double coin;
+        Random random = new Random();
+        while (heads < n) {
+            coin = random.nextDouble();
+            if (coin < 0.5) {
+                System.out.println("Tail");
+                fliping = fliping + 1;
+                heads = 0;
+            } else {
+                System.out.println("Head");
+                heads = heads + 1;
+            }
+            fliping = fliping + 1;
+
+        }
+     }
+
+     public static void clock(){
+        int counter = 0;
+         for ( ; ; ){
+            LocalDateTime now = LocalDateTime.now();
+
+            int second = now.getSecond();
+            String time = now.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+            if(counter != second) {
+                System.out.println("time is "+time);
+                counter = second;
+            }
+        }
+     }
+
+
 }
